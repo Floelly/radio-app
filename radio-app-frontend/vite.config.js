e16b3d16725f7e7944aabd7abe6daf61b8bea1e7
Floelly <email@florianbowitz.de>
2026-02-03 12:16:11 +0100

    implemented testing suite vitest + react testing

--- radio-app-frontend/vite.config.js
+++ radio-app-frontend/vite.config.js
@@ -2,18 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vite.dev/config/ + aliases + test-env
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.js",
+  },
   resolve: {
     alias: {
-      "@": "/src",
       "@api": "/src/api",
       "@components": "/src/components",
       "@views": "/src/views",
       "@assets": "/src/assets",
       "@context": "/src/context",
       "@config": "/src/config/config.js",
+      "@": "/src",
     },
   },
 });
